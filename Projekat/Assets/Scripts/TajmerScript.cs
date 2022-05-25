06ef6e3fe01d8f20a651239a031e2b52bfc75ca5
adameric1119s <72572019+adameric1119s@users.noreply.github.com>
2022-05-25 21:06:18 +0200

    Backend 1
    Povezanost izmedju scena, funkcionalnost button-a, tajmer za kviz

---
+++ Projekat/Assets/Scripts/TajmerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TajmerScript : MonoBehaviour
+{
+    // bool TimerOn = false;
+    float currentTime = 0;
+    float startingTime = 10;
+
+    public Text txtTajmer;
+  
+
+    void Start()
+    {
+        // TimerOn = true;
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            txtTajmer.text = currentTime.ToString("0");
+        } else
+            SceneManager.LoadScene(3);
+    }
+}
